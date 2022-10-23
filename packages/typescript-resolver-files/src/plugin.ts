54a1a913d5da1701b054dbcb97ffe5847dbb260e
Eddy Nguyen <ch@eddeee888.me>
2022-10-23 16:12:24 +1100

    Handle root fields

--- packages/typescript-resolver-files/src/plugin.ts
+++ packages/typescript-resolver-files/src/plugin.ts
@@ -1,9 +1,19 @@
 import { PluginFunction } from '@graphql-codegen/plugin-helpers';
 import { mkdir, writeFile } from 'fs/promises';
+import type { GraphQLObjectType } from 'graphql';
 import * as path from 'path';
 
 interface PluginConfig {
   resolverTypesPath: string;
+}
+
+interface File {
+  content: string;
+}
+
+interface Result {
+  dirs: string[];
+  files: Record<string, File>;
 }
 
 export const plugin: PluginFunction<PluginConfig> = async (
@@ -13,54 +23,54 @@ export const plugin: PluginFunction<PluginConfig> = async (
   info
 ) => {
   // TODO: check if there's a better way to handle output dir
-  const outputDir = info?.outputFile;
-  if (!outputDir) {
+  const baseOutputDir = info?.outputFile;
+  if (!baseOutputDir) {
     throw new Error('Output folder is invalid');
   }
   const { resolverTypesPath } = config;
+  const completeResolverTypesPath = path.join(baseOutputDir, resolverTypesPath);
 
-  const completeResolverTypesPath = `${outputDir}/${resolverTypesPath}`;
+  const result: Result = {
+    dirs: [],
+    files: {},
+  };
 
-  const dirs: string[] = [];
-  const files: Record<string, File> = {};
+  // Handle Query fields
+  parseRootGraphQLType(
+    {
+      baseOutputDir,
+      completeResolverTypesPath,
+      objectType: schema.getQueryType(),
+    },
+    result
+  );
 
-  const queryType = schema.getQueryType();
-  if (queryType) {
-    const queryFields = queryType.getFields();
+  // Handle Mutation fields
+  parseRootGraphQLType(
+    {
+      baseOutputDir,
+      completeResolverTypesPath,
+      objectType: schema.getMutationType(),
+    },
+    result
+  );
 
-    const outputQueryDir = `${outputDir}/Query`;
-    dirs.push(outputQueryDir);
-
-    Object.keys(queryFields).forEach((fieldName) => {
-      const fieldFilePath = `${outputQueryDir}/${fieldName}.ts`;
-      if (files[fieldFilePath]) {
-        throw new Error(
-          `Unexpected duplication in field filename: Query.${fieldFilePath}`
-        );
-      }
-
-      const relativePathToResolverTypes = path.relative(
-        outputQueryDir,
-        completeResolverTypesPath
-      );
-
-      files[fieldFilePath] = {
-        content: `import type { QueryResolvers } from '${printImportModule(
-          relativePathToResolverTypes
-        )}';
-        export const ${fieldName}: QueryResolvers['${fieldName}'] = async (_parent, _arg, _ctx) => {
-          /* Implement Query.${fieldName} logic here */
-        };`,
-      };
-    });
-  }
+  // Handle Subscription fields
+  parseRootGraphQLType(
+    {
+      baseOutputDir,
+      completeResolverTypesPath,
+      objectType: schema.getSubscriptionType(),
+    },
+    result
+  );
 
   // Write dirs and files
   await Promise.all(
-    dirs.map(async (dir) => await mkdir(dir, { recursive: true }))
+    result.dirs.map(async (dir) => await mkdir(dir, { recursive: true }))
   );
   await Promise.all(
-    Object.entries(files).map(
+    Object.entries(result.files).map(
       async ([filePath, file]) => await writeFile(filePath, file.content)
     )
   );
@@ -68,11 +78,55 @@ export const plugin: PluginFunction<PluginConfig> = async (
   return { content: '' };
 };
 
-interface File {
-  content: string;
-}
+/* GraphQL stuff */
+type ParseRootGraphQLType = (
+  params: {
+    baseOutputDir: string;
+    completeResolverTypesPath: string;
+    objectType?: GraphQLObjectType | null;
+  },
+  result: Result
+) => void;
+const parseRootGraphQLType: ParseRootGraphQLType = (
+  { objectType, baseOutputDir, completeResolverTypesPath },
+  result
+) => {
+  if (!objectType) {
+    return;
+  }
 
-/* Utils */
+  const typeName = objectType.name; // Query, Mutation, Subscription
+  const fields = objectType.getFields();
+  const outputDir = path.join(baseOutputDir, typeName);
+
+  result.dirs.push(outputDir);
+
+  Object.keys(fields).forEach((fieldName) => {
+    const fieldFilePath = path.join(outputDir, `${fieldName}.ts`);
+    if (result.files[fieldFilePath]) {
+      throw new Error(
+        `Unexpected duplication in field filename. Type: ${typeName}, file: ${fieldFilePath}`
+      );
+    }
+
+    const relativePathToResolverTypes = path.relative(
+      outputDir,
+      completeResolverTypesPath
+    );
+    const resolverTypeName = `${typeName}Resolvers`; // Generated type from typescript-resolvers plugin
+
+    result.files[fieldFilePath] = {
+      content: `import type { ${resolverTypeName} } from '${printImportModule(
+        relativePathToResolverTypes
+      )}';
+        export const ${fieldName}: ${resolverTypeName}['${fieldName}'] = async (_parent, _arg, _ctx) => {
+          /* Implement ${typeName}.${fieldName} logic here */
+        };`,
+    };
+  });
+};
+
+/* File Utils */
 const printImportModule = (moduleName: string) => {
   if (moduleName.endsWith('.ts')) {
     return moduleName.split('.').slice(0, -1).join('.');
