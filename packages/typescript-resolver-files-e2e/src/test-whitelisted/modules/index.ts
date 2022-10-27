f9aafe66f4d13ce25e58c2cc609bec01cba2b416
Eddy Nguyen <ch@eddeee888.me>
2022-10-27 21:31:59 +1100

    Set up e2e tests for whitelisted config

---
+++ packages/typescript-resolver-files-e2e/src/test-whitelisted/modules/index.ts
@@ -0,0 +1,18 @@
+/* This file was automatically generated. DO NOT UPDATE MANUALLY. */
+import type { Resolvers } from './types.generated';
+import { PaginationResult } from './base/resolvers/PaginationResult';
+import { me } from './user/resolvers/Query/me';
+import { userByAccountName } from './user/resolvers/Query/userByAccountName';
+import { StandardError } from './base/resolvers/StandardError';
+import { User } from './user/resolvers/User';
+import { UserPayload } from './user/resolvers/UserPayload';
+import { UserResult } from './user/resolvers/UserResult';
+export const resolvers: Resolvers = {
+  Query: { me, userByAccountName },
+
+  PaginationResult,
+  StandardError,
+  User,
+  UserPayload,
+  UserResult,
+};
---
+++ packages/typescript-resolver-files-e2e/src/test-whitelisted/modules/user/resolvers/Query/me.ts
@@ -0,0 +1,4 @@
+import type { QueryResolvers } from './../../../types.generated';
+export const me: QueryResolvers['me'] = async (_parent, _arg, _ctx) => {
+  /* Implement Query.me resolver logic here */
+};
