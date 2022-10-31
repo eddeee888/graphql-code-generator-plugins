0481a79e3f163e51a659ffb67549c75c119d4a24
Eddy Nguyen <ch@eddeee888.me>
2022-10-31 18:10:59 +1100

    Generate project.json new lines

--- tools/generators/typescript-resolver-files-add-e2e-test/index.ts
+++ tools/generators/typescript-resolver-files-add-e2e-test/index.ts
@@ -1,16 +1,46 @@
 import * as path from 'path';
-import { Tree, formatFiles, generateFiles } from '@nrwl/devkit';
+import * as fs from 'fs';
+import {
+  Tree,
+  formatFiles,
+  generateFiles,
+  ProjectConfiguration,
+  readProjectConfiguration,
+  updateProjectConfiguration,
+} from '@nrwl/devkit';
 import type { TypescriptResolverFilesAddE2ETestSchema } from './schema';
 
-type NormalizedSchema = TypescriptResolverFilesAddE2ETestSchema;
-
-function normalizeOptions(
-  options: TypescriptResolverFilesAddE2ETestSchema
-): NormalizedSchema {
-  return { ...options };
+interface NormalizedSchema extends TypescriptResolverFilesAddE2ETestSchema {
+  projectName: string;
+  projectConfig: ProjectConfiguration;
+  testFullName: string;
 }
 
-function addFiles(tree: Tree, options: NormalizedSchema) {
+const projectName = 'typescript-resolver-files-e2e';
+
+const normalizeOptions = (
+  tree: Tree,
+  options: TypescriptResolverFilesAddE2ETestSchema
+): NormalizedSchema => {
+  const testFullName = `test-${options.testName}`;
+  const testDir = path.join('packages', projectName, 'src', testFullName);
+  if (fs.existsSync(testDir)) {
+    throw new Error(
+      `${testFullName} already exists. Try a different testName.`
+    );
+  }
+
+  const projectConfig = readProjectConfiguration(tree, projectName);
+
+  return {
+    ...options,
+    projectName,
+    projectConfig,
+    testFullName,
+  };
+};
+
+const addFiles = (tree: Tree, options: NormalizedSchema) => {
   generateFiles(
     tree,
     path.join(__dirname, 'files'),
@@ -20,13 +50,38 @@ function addFiles(tree: Tree, options: NormalizedSchema) {
       template: '',
     }
   );
-}
+};
+
+const updateProjectConfig = (tree: Tree, options: NormalizedSchema): void => {
+  const commands: string[] =
+    options.projectConfig.targets?.['e2e-run']?.options?.['commands'];
+  if (!commands) {
+    throw new Error('Unable to find e2e-run commands in project.json.');
+  }
+  const graphQLCodegenConfig =
+    options.projectConfig.targets?.['graphql-codegen']?.configurations;
+  if (!graphQLCodegenConfig) {
+    throw new Error(
+      'Unable to find graphql-codegen configurations in project.json.'
+    );
+  }
+
+  commands.push(
+    `nx graphql-codegen typescript-resolver-files-e2e -c ${options.testFullName}`
+  );
+  graphQLCodegenConfig[options.testFullName] = {
+    configFile: `packages/typescript-resolver-files-e2e/src/${options.testFullName}/codegen.yml`,
+  };
+
+  updateProjectConfiguration(tree, options.projectName, options.projectConfig);
+};
 
 export default async function (
   tree: Tree,
   options: TypescriptResolverFilesAddE2ETestSchema
 ) {
-  const normalizedOptions = normalizeOptions(options);
+  const normalizedOptions = normalizeOptions(tree, options);
   addFiles(tree, normalizedOptions);
+  updateProjectConfig(tree, normalizedOptions);
   await formatFiles(tree);
 }
