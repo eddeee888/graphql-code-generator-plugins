47dbea24e8c8c5030b11f8a9219f91742a8b228f
Eddy Nguyen <ch@eddeee888.me>
2022-11-01 17:50:51 +1100

    Update e2e tests for Scalars

--- packages/typescript-resolver-files-e2e/src/test-whitelisted/modules/index.ts
+++ packages/typescript-resolver-files-e2e/src/test-whitelisted/modules/index.ts
@@ -1,5 +1,6 @@
 /* This file was automatically generated. DO NOT UPDATE MANUALLY. */
 import type { Resolvers } from './types.generated';
+import { DateTime } from './base/resolvers/DateTime';
 import { PaginationResult } from './base/resolvers/PaginationResult';
 import { me as Query_me } from './user/resolvers/Query/me';
 import { userByAccountName as Query_userByAccountName } from './user/resolvers/Query/userByAccountName';
@@ -10,6 +11,7 @@ import { UserResult } from './user/resolvers/UserResult';
 export const resolvers: Resolvers = {
   Query: { me: Query_me, userByAccountName: Query_userByAccountName },
 
+  DateTime: DateTime,
   PaginationResult: PaginationResult,
   StandardError: StandardError,
   User: User,
