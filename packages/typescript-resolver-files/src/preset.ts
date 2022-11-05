cfe4c45d9aca2ac9f7320a157bc2ea24ffb3c6f4
Eddy Nguyen <ch@eddeee888.me>
2022-11-05 22:30:37 +1100

    Add validate/parse function

--- packages/typescript-resolver-files/src/preset.ts
+++ packages/typescript-resolver-files/src/preset.ts
@@ -5,22 +5,22 @@ import { parseSources } from './utils';
 import { RunContext } from './types';
 import { run } from './run';
 
-interface PresetConfig {
+interface ParsedPresetConfig {
   resolverTypesPath: string;
-  relativeTargetDir?: string;
-  mainFile?: string;
-  mode?: 'merged' | 'modules';
-  whitelistedModules?: string[];
-  externalResolvers?: Record<string, string>;
+  relativeTargetDir: string;
+  mainFile: string;
+  mode: 'merged' | 'modules';
+  whitelistedModules: string[];
+  externalResolvers: Record<string, string>;
 }
 
-const configDefaultMode = 'modules';
+const presetName = '@eddeee888/gcg-typescript-resolver-files';
 
-export const preset: Types.OutputPreset<PresetConfig> = {
+export const preset: Types.OutputPreset<ParsedPresetConfig> = {
   buildGeneratesSection: ({
     schema,
     schemaAst,
-    presetConfig,
+    presetConfig: rawPresetConfig,
     baseOutputDir,
   }) => {
     if (!schemaAst) {
@@ -37,12 +37,12 @@ export const preset: Types.OutputPreset<PresetConfig> = {
 
     const {
       resolverTypesPath: relativeResolverTypesPathFromBaseOutputDir,
-      relativeTargetDir = '',
-      mainFile = 'index.ts',
-      mode = configDefaultMode,
-      whitelistedModules = [],
-      externalResolvers = {},
-    } = presetConfig;
+      relativeTargetDir,
+      mainFile,
+      mode,
+      whitelistedModules,
+      externalResolvers,
+    } = validatePresetConfig(rawPresetConfig);
 
     const resolverTypesPath = path.join(
       baseOutputDir,
@@ -78,4 +78,62 @@ export const preset: Types.OutputPreset<PresetConfig> = {
       documents: [],
     }));
   },
+};
+
+interface RawPresetConfig {
+  resolverTypesPath?: string;
+  relativeTargetDir?: string;
+  mainFile?: string;
+  mode?: string;
+  whitelistedModules?: string[];
+  externalResolvers?: Record<string, string>;
+}
+const validatePresetConfig = ({
+  resolverTypesPath,
+  relativeTargetDir = '',
+  mainFile = 'index.ts',
+  mode = 'modules',
+  whitelistedModules,
+  externalResolvers = {},
+}: RawPresetConfig): ParsedPresetConfig => {
+  if (!resolverTypesPath) {
+    throw new Error(
+      `Validation Error - ${presetName} - presetConfig.resolverTypesPath is required`
+    );
+  }
+
+  if (path.extname(mainFile) === '') {
+    throw new Error(
+      `Validation Error - ${presetName} - presetConfig.mainFile must be a valid file name`
+    );
+  }
+
+  if (mode !== 'merged' && mode !== 'modules') {
+    throw new Error(
+      `Validation Error - ${presetName} - presetConfig.mode must be "merged" or "modules" (default is "modules")`
+    );
+  }
+
+  if (whitelistedModules) {
+    if (!Array.isArray(whitelistedModules)) {
+      throw new Error(
+        `Validation Error - ${presetName} - presetConfig.whitelistedModules must be an array if provided`
+      );
+    }
+
+    if (mode !== 'modules') {
+      throw new Error(
+        `Validation Error - ${presetName} - presetConfig.whitelistedModules can only be used with presetConfig.mode == "modules"`
+      );
+    }
+  }
+
+  return {
+    resolverTypesPath,
+    relativeTargetDir,
+    mainFile,
+    mode: mode,
+    whitelistedModules: whitelistedModules || [],
+    externalResolvers,
+  };
 };
