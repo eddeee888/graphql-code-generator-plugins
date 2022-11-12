0792b5e7efc96b38e89cb2ef386e3a4b5d8eab61
Eddy Nguyen <ch@eddeee888.me>
2022-11-12 20:02:08 +1100

    Setup config overrides tests

---
+++ packages/typescript-resolver-files-e2e/src/test-config-overrides/modules/user/resolvers/Query/userByAccountName.ts
@@ -0,0 +1,8 @@
+import type { QueryResolvers } from './../../../types.generated';
+export const userByAccountName: QueryResolvers['userByAccountName'] = async (
+  _parent,
+  _arg,
+  _ctx
+) => {
+  /* Implement Query.userByAccountName resolver logic here */
+};
