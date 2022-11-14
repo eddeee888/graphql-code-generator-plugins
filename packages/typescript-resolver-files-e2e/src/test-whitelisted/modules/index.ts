49ba3958e2275e2d1c9195111f2e3349ee437310
Eddy Nguyen <ch@eddeee888.me>
2022-11-15 00:21:55 +1100

    Update e2e

--- packages/typescript-resolver-files-e2e/src/test-whitelisted/modules/index.ts
+++ packages/typescript-resolver-files-e2e/src/test-whitelisted/modules/index.ts
@@ -1,20 +1,10 @@
 /* This file was automatically generated. DO NOT UPDATE MANUALLY. */
 import type { Resolvers } from './types.generated';
 import { PaginationResult } from './base/resolvers/PaginationResult';
-import { me as Query_me } from './user/resolvers/Query/me';
-import { userByAccountName as Query_userByAccountName } from './user/resolvers/Query/userByAccountName';
 import { StandardError } from './base/resolvers/StandardError';
-import { User } from './user/resolvers/User';
-import { UserPayload } from './user/resolvers/UserPayload';
-import { UserResult } from './user/resolvers/UserResult';
 import { DateTimeResolver } from 'graphql-scalars';
 export const resolvers: Resolvers = {
-  Query: { me: Query_me, userByAccountName: Query_userByAccountName },
-
   PaginationResult: PaginationResult,
   StandardError: StandardError,
-  User: User,
-  UserPayload: UserPayload,
-  UserResult: UserResult,
   DateTime: DateTimeResolver,
 };
--- packages/typescript-resolver-files-e2e/src/test-whitelisted/modules/user/resolvers/Query/userByAccountName.ts
+++
@@ -1,8 +0,0 @@
-import type { QueryResolvers } from './../../../types.generated';
-export const userByAccountName: QueryResolvers['userByAccountName'] = async (
-  _parent,
-  _arg,
-  _ctx
-) => {
-  /* Implement Query.userByAccountName resolver logic here */
-};
