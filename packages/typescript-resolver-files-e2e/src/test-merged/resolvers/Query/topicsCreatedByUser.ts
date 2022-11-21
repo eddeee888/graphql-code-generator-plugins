33db8f3679e750e943fbf9756aec58adc27c6ad5
Eddy Nguyen <ch@eddeee888.me>
2022-11-21 22:28:35 +1100

    Update e2e tests

--- packages/typescript-resolver-files-e2e/src/test-merged/resolvers/Query/topicsCreatedByUser.ts
+++ packages/typescript-resolver-files-e2e/src/test-merged/resolvers/Query/topicsCreatedByUser.ts
@@ -1,5 +1,6 @@
 import type { QueryResolvers } from './../types.generated';
-export const topicsCreatedByUser: QueryResolvers['topicsCreatedByUser'] =
-  async (_parent, _arg, _ctx) => {
-    /* Implement Query.topicsCreatedByUser resolver logic here */
-  };
+export const topicsCreatedByUser: NonNullable<
+  QueryResolvers['topicsCreatedByUser']
+> = async (_parent, _arg, _ctx) => {
+  /* Implement Query.topicsCreatedByUser resolver logic here */
+};
--- packages/typescript-resolver-files-e2e/src/test-modules/modules/user/resolvers/Subscription/profileChanges.ts
+++ packages/typescript-resolver-files-e2e/src/test-modules/modules/user/resolvers/Subscription/profileChanges.ts
@@ -1,8 +1,6 @@
 import type { SubscriptionResolvers } from './../../../types.generated';
-export const profileChanges: SubscriptionResolvers['profileChanges'] = async (
-  _parent,
-  _arg,
-  _ctx
-) => {
+export const profileChanges: NonNullable<
+  SubscriptionResolvers['profileChanges']
+> = async (_parent, _arg, _ctx) => {
   /* Implement Subscription.profileChanges resolver logic here */
 };
