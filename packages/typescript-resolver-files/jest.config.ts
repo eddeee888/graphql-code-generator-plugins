8d567c744ad0b6b59711283616e4d5e2cbade7f2
Eddy Nguyen <ch@eddeee888.me>
2023-03-05 11:31:32 +1100

    Apply jest config

--- packages/typescript-resolver-files/jest.config.ts
+++ packages/typescript-resolver-files/jest.config.ts
@@ -2,14 +2,15 @@
 export default {
   displayName: 'typescript-resolver-files',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/typescript-resolver-files',
