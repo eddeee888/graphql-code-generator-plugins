982d7062dd59c345a1ef550b13edcbb89de0656c
Eddy Nguyen <ch@eddeee888.me>
2023-03-11 21:46:34 +1100

    Rebase e2e

--- packages/typescript-resolver-files-e2e/src/test-external-resolvers/modules/user/resolvers/Subscription/profileChanges.ts
+++ packages/typescript-resolver-files-e2e/src/test-external-resolvers/modules/user/resolvers/Subscription/profileChanges.ts
@@ -1,6 +1,8 @@
 import type { SubscriptionResolvers } from './../../../types.generated';
 export const profileChanges: NonNullable<
   SubscriptionResolvers['profileChanges']
-> = async (_parent, _arg, _ctx) => {
-  /* Implement Subscription.profileChanges resolver logic here */
+> = {
+  subscribe: async (_parent, _arg, _ctx) => {
+    /* Implement Subscription.profileChanges resolver logic here */
+  },
 };
