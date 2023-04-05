3505baa3189aa4d60013cd5c659675281558269e
Eddy Nguyen <ch@eddeee888.me>
2023-04-05 19:42:42 +1000

    Remove hack whilst waiting for plugin-helpers

--- packages/typescript-resolver-files/src/defineConfig.ts
+++ packages/typescript-resolver-files/src/defineConfig.ts
@@ -3,16 +3,10 @@ import type { Types } from '@graphql-codegen/plugin-helpers';
 import type { TypedPresetConfig } from './validatePresetConfig';
 import { preset } from './preset';
 
-// TODO: remove this once new @graphql-codegen/plugin-helpers is released
-type ConfigWatchPattern = {
-  watchPattern?: string | string[];
-};
-
 export const defineConfig = (
   presetConfig: TypedPresetConfig = {},
   context: { baseOutputDir: string } = { baseOutputDir: '' }
-): Pick<Types.ConfiguredOutput, 'preset' | 'presetConfig'> &
-  ConfigWatchPattern => {
+): Pick<Types.ConfiguredOutput, 'preset' | 'presetConfig' | 'watchPattern'> => {
   const { baseOutputDir } = context;
 
   const mappersFileExtension =
