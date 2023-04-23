6a3c01b5b8f4364c29ea05f4c07108a5edee8103
Eddy Nguyen <ch@eddeee888.me>
2023-04-23 21:43:55 +1000

    Allows overriding native scalar types' type (Equivalent of typescript plugin's scalars option) (#137)

--- packages/typescript-resolver-files-e2e/src/test-scalars-module/schema-overrides/topic/resolvers/Topic.ts
+++ packages/typescript-resolver-files-e2e/src/test-scalars-module/schema-overrides/topic/resolvers/Topic.ts
@@ -4,4 +4,8 @@ export const Topic: TopicResolvers = {
   creator: () => {
     /* Topic.creator resolver is required because Topic.creator exists but TopicMapper.creator does not */
   },
+  id: ({ id }) => {
+    /* Topic.id resolver is required because Topic.id and TopicMapper.id are not compatible */
+    return id;
+  },
 };
