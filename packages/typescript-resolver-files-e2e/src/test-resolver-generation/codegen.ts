412c1c8c8b4d8050022053d14ddf6539fda376b1
Eddy Nguyen <ch@eddeee888.me>
2023-06-25 17:36:56 +1000

    Baseline e2e tests

--- packages/typescript-resolver-files-e2e/src/test-resolver-generation/codegen.ts
+++ packages/typescript-resolver-files-e2e/src/test-resolver-generation/codegen.ts
@@ -2,22 +2,36 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 import { defineConfig } from '../../../../dist/packages/typescript-resolver-files/src/index';
 
 const config: CodegenConfig = {
-  schema: [
-    'packages/typescript-resolver-files-e2e/src/test-resolver-generation/**/*.graphqls',
-    'packages/typescript-resolver-files-e2e/src/test-resolver-generation/**/*.graphqls.ts',
-  ],
   hooks: {
     afterAllFileWrite: ['prettier --write'],
   },
   generates: {
     'packages/typescript-resolver-files-e2e/src/test-resolver-generation/schema-disabled':
-      defineConfig({ resolverGeneration: 'disabled' }),
+      {
+        schema: [
+          'packages/typescript-resolver-files-e2e/src/test-resolver-generation/schema-disabled/**/*.graphqls',
+          'packages/typescript-resolver-files-e2e/src/test-resolver-generation/schema-disabled/**/*.graphqls.ts',
+        ],
+        ...defineConfig({ resolverGeneration: 'disabled' }),
+      },
 
     'packages/typescript-resolver-files-e2e/src/test-resolver-generation/schema-recommended':
-      defineConfig({ resolverGeneration: 'recommended' }),
+      {
+        schema: [
+          'packages/typescript-resolver-files-e2e/src/test-resolver-generation/schema-recommended/**/*.graphqls',
+          'packages/typescript-resolver-files-e2e/src/test-resolver-generation/schema-recommended/**/*.graphqls.ts',
+        ],
+        ...defineConfig({ resolverGeneration: 'recommended' }),
+      },
 
     'packages/typescript-resolver-files-e2e/src/test-resolver-generation/schema-full':
-      defineConfig({ resolverGeneration: 'full' }),
+      {
+        schema: [
+          'packages/typescript-resolver-files-e2e/src/test-resolver-generation/schema-full/**/*.graphqls',
+          'packages/typescript-resolver-files-e2e/src/test-resolver-generation/schema-full/**/*.graphqls.ts',
+        ],
+        ...defineConfig({ resolverGeneration: 'full' }),
+      },
   },
 };
 
