2205e3f510154fb5ea2e97bb9a9a6da29394bd13
Eddy Nguyen <ch@eddeee888.me>
2023-06-29 21:16:17 +1000

    Fix default custom scalar template (#174)
    * Update handleGraphQLScalarType
    
    * Regen e2e
    
    * Add changeset

---
+++ .changeset/wise-snails-visit.md
@@ -0,0 +1,5 @@
+---
+'@eddeee888/gcg-typescript-resolver-files': patch
+---
+
+Update default custom GraphQLScalar template to guide users better
--- packages/typescript-resolver-files-e2e/src/test-scalars-module/schema/base/resolvers/DateTime.ts
+++ packages/typescript-resolver-files-e2e/src/test-scalars-module/schema/base/resolvers/DateTime.ts
@@ -1,4 +1,14 @@
-import type { GraphQLScalarType } from 'graphql';
-export const DateTime: GraphQLScalarType = {
-  /* Implement DateTime scalar logic here */
-};
+import { GraphQLScalarType } from 'graphql';
+export const DateTime = new GraphQLScalarType({
+  name: 'DateTime',
+  description: 'DateTime description',
+  serialize: (value) => {
+    /* Implement logic to turn the returned value from resolvers to a value that can be sent to clients */
+  },
+  parseValue: (value) => {
+    /* Implement logic to parse input that was sent to the server as variables */
+  },
+  parseLiteral: (ast) => {
+    /* Implement logic to parse input that was sent to the server as literal values (string, number, or boolean) */
+  },
+});
