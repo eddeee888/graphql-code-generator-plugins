958c3be69b3f3bfc47cd757213824a0880467aca
Eddy Nguyen <ch@eddeee888.me>
2023-07-08 20:37:52 +1000

    Make server preset have the same config as server-config

--- packages/typescript-resolver-files/jest.config.ts
+++ packages/typescript-resolver-files/jest.config.ts
@@ -2,16 +2,10 @@
 export default {
   displayName: 'typescript-resolver-files',
   preset: '../../jest.preset.js',
-  globals: {},
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]sx?$': [
-      'ts-jest',
-      {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
-      },
-    ],
+    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/packages/typescript-resolver-files',
 };
