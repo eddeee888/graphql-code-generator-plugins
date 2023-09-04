03de7991cf7ba5579986348c21b6a0f7d762c492
Eddy Nguyen <ch@eddeee888.me>
2023-09-04 21:43:36 +1000

    Implement add for generated resolverTypes file only

--- packages/typescript-resolver-files/src/addVirtualTypesFileToTsMorphProject/addVirtualTypesFileToTsMorphProject.ts
+++ packages/typescript-resolver-files/src/addVirtualTypesFileToTsMorphProject/addVirtualTypesFileToTsMorphProject.ts
@@ -1,3 +1,6 @@
+import type { Types } from '@graphql-codegen/plugin-helpers';
+import * as addPlugin from '@graphql-codegen/add';
+import type { AddPluginConfig } from '@graphql-codegen/add/typings/config';
 import * as typeScriptPlugin from '@graphql-codegen/typescript';
 import * as typeScriptResolversPlugin from '@graphql-codegen/typescript-resolvers';
 import type { GraphQLSchema } from 'graphql';
@@ -9,16 +12,19 @@ export const addVirtualTypesFileToTsMorphProject = async ({
   schemaAst,
   resolverTypesPath,
   resolverTypesConfig,
+  addConfig,
 }: {
   tsMorphProject: Project;
   schemaAst: GraphQLSchema;
   resolverTypesPath: string;
   resolverTypesConfig: ServerConfig;
+  addConfig?: AddPluginConfig;
 }): Promise<SourceFile> => {
   const typesFile = await generateVirtualTypesFile({
     schemaAst,
     resolverTypesPath,
     resolverTypesConfig,
+    addConfig,
   });
 
   const typesSourceFile = tsMorphProject.createSourceFile(
@@ -38,24 +44,51 @@ const generateVirtualTypesFile = async ({
   schemaAst,
   resolverTypesPath,
   resolverTypesConfig,
+  addConfig,
 }: {
   schemaAst: GraphQLSchema;
   resolverTypesPath: string;
   resolverTypesConfig: ServerConfig;
+  addConfig?: AddPluginConfig;
 }): Promise<{
   filePath: string;
   content: string;
 }> => {
-  const [typescriptResult, typescriptResolversResult] = await Promise.all([
-    typeScriptPlugin.plugin(schemaAst, [], resolverTypesConfig),
-    typeScriptResolversPlugin.plugin(schemaAst, [], resolverTypesConfig),
-  ]);
+  const [typescriptResult, typescriptResolversResult, addResult] =
+    await Promise.all([
+      typeScriptPlugin.plugin(schemaAst, [], resolverTypesConfig),
+      typeScriptResolversPlugin.plugin(schemaAst, [], resolverTypesConfig),
+      addConfig ? addPlugin.plugin(schemaAst, [], addConfig) : '',
+    ]);
+
+  const addResultAsComplextOutput =
+    convertPluginOutputToComplextPluginOutput(addResult);
 
   return {
     filePath: resolverTypesPath,
     content: `
+    ${addResultAsComplextOutput.prepend?.join('\n')}
     ${typescriptResult.prepend?.join('\n')}
     ${typescriptResolversResult.prepend?.join('\n')}
-    ${typescriptResult.content}${typescriptResolversResult.content}`,
+    ${typescriptResult.content}
+    ${typescriptResolversResult.content}
+    ${addResultAsComplextOutput.content}
+    ${addResultAsComplextOutput.append?.join('\n')}
+    `,
   };
+};
+
+const convertPluginOutputToComplextPluginOutput = (
+  output: Types.PluginOutput
+): Types.ComplexPluginOutput => {
+  if (typeof output === 'string') {
+    return {
+      content: output || '',
+    };
+  }
+
+  if (Array.isArray(output.content)) {
+    output.content = output.content.join('\n');
+  }
+  return output;
 };
