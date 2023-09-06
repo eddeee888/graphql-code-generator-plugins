2bf7b5737460ff8119b86ece759b98b5ff4079d8
Eddy Nguyen <enguyen@seek.com.au>
2023-09-06 23:45:07 +1000

    Rebase e2e

--- packages/typescript-resolver-files-e2e/src/test-extended-object-types/schema-base/types.generated.ts
+++ packages/typescript-resolver-files-e2e/src/test-extended-object-types/schema-base/types.generated.ts
@@ -60,11 +60,11 @@ export type Mutation = {
   topicEdit: TopicEditPayload;
 };
 
-export type MutationTopicCreateArgs = {
+export type MutationtopicCreateArgs = {
   input: TopicCreateInput;
 };
 
-export type MutationTopicEditArgs = {
+export type MutationtopicEditArgs = {
   input: TopicEditInput;
 };
 
@@ -99,15 +99,15 @@ export type Query = {
   userByAccountName: UserPayload;
 };
 
-export type QueryTopicByIdArgs = {
+export type QuerytopicByIdArgs = {
   id: Scalars['ID']['input'];
 };
 
-export type QueryTopicsCreatedByUserArgs = {
+export type QuerytopicsCreatedByUserArgs = {
   input: TopicsCreatedByUserInput;
 };
 
-export type QueryUserByAccountNameArgs = {
+export type QueryuserByAccountNameArgs = {
   accountName: Scalars['String']['input'];
 };
 
@@ -465,13 +465,13 @@ export type MutationResolvers<
     ResolversTypes['TopicCreatePayload'],
     ParentType,
     ContextType,
-    RequireFields<MutationTopicCreateArgs, 'input'>
+    RequireFields<MutationtopicCreateArgs, 'input'>
   >;
   topicEdit?: Resolver<
     ResolversTypes['TopicEditPayload'],
     ParentType,
     ContextType,
-    RequireFields<MutationTopicEditArgs, 'input'>
+    RequireFields<MutationtopicEditArgs, 'input'>
   >;
 };
 
@@ -511,19 +511,19 @@ export type QueryResolvers<
     ResolversTypes['TopicByIdPayload'],
     ParentType,
     ContextType,
-    RequireFields<QueryTopicByIdArgs, 'id'>
+    RequireFields<QuerytopicByIdArgs, 'id'>
   >;
   topicsCreatedByUser?: Resolver<
     ResolversTypes['TopicsCreatedByUserPayload'],
     ParentType,
     ContextType,
-    RequireFields<QueryTopicsCreatedByUserArgs, 'input'>
+    RequireFields<QuerytopicsCreatedByUserArgs, 'input'>
   >;
   userByAccountName?: Resolver<
     ResolversTypes['UserPayload'],
     ParentType,
     ContextType,
-    RequireFields<QueryUserByAccountNameArgs, 'accountName'>
+    RequireFields<QueryuserByAccountNameArgs, 'accountName'>
   >;
 };
 
