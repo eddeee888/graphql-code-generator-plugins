c56ff397493b990155b4b241bd95ab22239e76c5
Eddy Nguyen <github@eddeee888.me>
2024-05-17 21:45:44 +1000

    Use afterOneFileWrite to avoid long commands that break for windows

--- packages/typescript-resolver-files-e2e/src/test-resolver-generation/codegen.ts
+++ packages/typescript-resolver-files-e2e/src/test-resolver-generation/codegen.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@eddeee888/gcg-typescript-resolver-files';
 
 const config: CodegenConfig = {
   hooks: {
-    afterAllFileWrite: ['prettier --write'],
+    afterOneFileWrite: ['prettier --write'],
   },
   generates: {
     'packages/typescript-resolver-files-e2e/src/test-resolver-generation/schema-custom-object':
