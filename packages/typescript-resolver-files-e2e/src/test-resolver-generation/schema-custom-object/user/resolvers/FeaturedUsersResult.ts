3374748c785999e61bca1d53accdcd569cc890cc
Eddy Nguyen <github@eddeee888.me>
2024-05-17 21:49:53 +1000

    Make note about mapper less intimidating

--- packages/typescript-resolver-files-e2e/src/test-resolver-generation/schema-custom-object/user/resolvers/FeaturedUsersResult.ts
+++ packages/typescript-resolver-files-e2e/src/test-resolver-generation/schema-custom-object/user/resolvers/FeaturedUsersResult.ts
@@ -1,14 +1,12 @@
 import type { FeaturedUsersResultResolvers } from './../../types.generated';
 /*
- * WARNING
- * This object type is NOT supposed to be generated because it is in the `resolverGeneration.object: "!*Result,!*Error"` ignore pattern.
+ * Note: This object type is generated because "FeaturedUsersResultMapper" is declared. This is to ensure runtime safety.
  *
- * However, it is generated because "FeaturedUsersResultMapper" is declared. This is to ensure runtime safety.
  * When a mapper is used, it is possible to hit runtime errors in some senarios:
  * - given a field name, the schema type's field type does not match mapper's field type
- * - a schema type's field does not exist in the mapper's fields
+ * - or a schema type's field does not exist in the mapper's fields
  *
- * If you want to skip this generation, remove the mapper or update the ignore pattern in the config.
+ * If you want to skip this file generation, remove the mapper or update the pattern in the `resolverGeneration.object` config.
  */
 export const FeaturedUsersResult: FeaturedUsersResultResolvers = {
   /* Implement FeaturedUsersResult resolver logic here */
--- packages/typescript-resolver-files-e2e/src/test-resolver-generation/schema-disabled/topic/resolvers/User.ts
+++ packages/typescript-resolver-files-e2e/src/test-resolver-generation/schema-disabled/topic/resolvers/User.ts
@@ -1,14 +1,12 @@
 import type { UserResolvers } from './../../types.generated';
 /*
- * WARNING
- * This object type is NOT supposed to be generated because it is in the `resolverGeneration.object: ""` ignore pattern.
+ * Note: This object type is generated because "UserMapper" is declared. This is to ensure runtime safety.
  *
- * However, it is generated because "UserMapper" is declared. This is to ensure runtime safety.
  * When a mapper is used, it is possible to hit runtime errors in some senarios:
  * - given a field name, the schema type's field type does not match mapper's field type
- * - a schema type's field does not exist in the mapper's fields
+ * - or a schema type's field does not exist in the mapper's fields
  *
- * If you want to skip this generation, remove the mapper or update the ignore pattern in the config.
+ * If you want to skip this file generation, remove the mapper or update the pattern in the `resolverGeneration.object` config.
  */
 export const User: Pick<UserResolvers, 'topics'> = {
   /* Implement User resolver logic here */
