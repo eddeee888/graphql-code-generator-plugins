33e1de14cf7355f48b84857ca85b72e150c9adbb
Eddy Nguyen <ch@eddeee888.me>
2024-05-18 17:55:35 +1000

    Add test for forced generation whenever there's a mapper for ignored object type (#273)
    * Add test for forced generation
    
    * Use afterOneFileWrite to avoid long commands that break for windows
    
    * Make note about mapper less intimidating

--- packages/typescript-resolver-files-e2e/src/test-resolver-generation/schema-disabled/resolvers.generated.ts
+++ packages/typescript-resolver-files-e2e/src/test-resolver-generation/schema-disabled/resolvers.generated.ts
@@ -1,8 +1,11 @@
 /* This file was automatically generated. DO NOT UPDATE MANUALLY. */
 import type { Resolvers } from './types.generated';
 import { Topic } from './topic/resolvers/Topic';
+import { User as user_User } from './user/resolvers/User';
+import { User as topic_User } from './topic/resolvers/User';
 import { DateTimeResolver } from 'graphql-scalars';
 export const resolvers: Resolvers = {
   Topic: Topic,
+  User: { ...user_User, ...topic_User },
   DateTime: DateTimeResolver,
 };
---
+++ packages/typescript-resolver-files-e2e/src/test-resolver-generation/schema-disabled/user/resolvers/User.ts
@@ -0,0 +1,44 @@
+import type { UserResolvers } from './../../types.generated';
+/*
+ * Note: This object type is generated because "UserMapper" is declared. This is to ensure runtime safety.
+ *
+ * When a mapper is used, it is possible to hit runtime errors in some senarios:
+ * - given a field name, the schema type's field type does not match mapper's field type
+ * - or a schema type's field does not exist in the mapper's fields
+ *
+ * If you want to skip this file generation, remove the mapper or update the pattern in the `resolverGeneration.object` config.
+ */
+export const User: Pick<
+  UserResolvers,
+  | 'accountGitHub'
+  | 'accountLinkedIn'
+  | 'accountName'
+  | 'accountTwitter'
+  | 'accountWebsite'
+  | 'avatar'
+  | 'id'
+  | 'name'
+> = {
+  /* Implement User resolver logic here */
+  accountGitHub: async (_parent, _arg, _ctx) => {
+    /* User.accountGitHub resolver is required because User.accountGitHub exists but UserMapper.accountGitHub does not */
+  },
+  accountLinkedIn: async (_parent, _arg, _ctx) => {
+    /* User.accountLinkedIn resolver is required because User.accountLinkedIn exists but UserMapper.accountLinkedIn does not */
+  },
+  accountName: async (_parent, _arg, _ctx) => {
+    /* User.accountName resolver is required because User.accountName exists but UserMapper.accountName does not */
+  },
+  accountTwitter: async (_parent, _arg, _ctx) => {
+    /* User.accountTwitter resolver is required because User.accountTwitter exists but UserMapper.accountTwitter does not */
+  },
+  accountWebsite: async (_parent, _arg, _ctx) => {
+    /* User.accountWebsite resolver is required because User.accountWebsite exists but UserMapper.accountWebsite does not */
+  },
+  avatar: async (_parent, _arg, _ctx) => {
+    /* User.avatar resolver is required because User.avatar exists but UserMapper.avatar does not */
+  },
+  name: async (_parent, _arg, _ctx) => {
+    /* User.name resolver is required because User.name exists but UserMapper.name does not */
+  },
+};
