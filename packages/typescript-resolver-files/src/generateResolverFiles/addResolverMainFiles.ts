7f465d571a6a3508999f3fcf1708ad9af55a2716
Eddy Nguyen <github@eddeee888.me>
2024-06-13 21:59:47 +1000

    Fix lint

--- packages/typescript-resolver-files/src/generateResolverFiles/addResolverMainFiles.ts
+++ packages/typescript-resolver-files/src/generateResolverFiles/addResolverMainFiles.ts
@@ -37,7 +37,6 @@ export const addResolverMainFiles = ({
     resolverMainFile,
     resolverMainFileMode,
     emitLegacyCommonJSImports,
-    generatedTypesFileMeta,
   },
   result,
 }: GenerateResolverFilesContext): void => {
