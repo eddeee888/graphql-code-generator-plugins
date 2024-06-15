a3c8a7a09e83759c25e592201b308568134e4382
Eddy Nguyen <github@eddeee888.me>
2024-06-15 13:23:41 +1000

    Update baseline tests

--- packages/typescript-resolver-files-e2e/src/test-resolver-generation/schema-disabled/topic/resolvers/Topic.ts
+++ packages/typescript-resolver-files-e2e/src/test-resolver-generation/schema-disabled/topic/resolvers/Topic.ts
@@ -2,7 +2,7 @@ import type { TopicResolvers } from './../../types.generated';
 /*
  * Note: This object type is generated because "TopicMapper" is declared. This is to ensure runtime safety.
  *
- * When a mapper is used, it is possible to hit runtime errors in some senarios:
+ * When a mapper is used, it is possible to hit runtime errors in some scenarios:
  * - given a field name, the schema type's field type does not match mapper's field type
  * - or a schema type's field does not exist in the mapper's fields
  *
