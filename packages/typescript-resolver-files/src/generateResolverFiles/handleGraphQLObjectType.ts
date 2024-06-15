c1995da20cbf8205afc6bff2492eb76c290e039a
Eddy Nguyen <github@eddeee888.me>
2024-06-15 13:21:43 +1000

    Fix typo

--- packages/typescript-resolver-files/src/generateResolverFiles/handleGraphQLObjectType.ts
+++ packages/typescript-resolver-files/src/generateResolverFiles/handleGraphQLObjectType.ts
@@ -54,7 +54,7 @@ export const handleGraphQLObjectType: GraphQLTypeHandler<
       return `/*
     * Note: This object type is generated because "${mapperDetails.typeMapperName}" is declared. This is to ensure runtime safety.
     *
-    * When a mapper is used, it is possible to hit runtime errors in some senarios:
+    * When a mapper is used, it is possible to hit runtime errors in some scenarios:
     * - given a field name, the schema type's field type does not match mapper's field type
     * - or a schema type's field does not exist in the mapper's fields
     *
