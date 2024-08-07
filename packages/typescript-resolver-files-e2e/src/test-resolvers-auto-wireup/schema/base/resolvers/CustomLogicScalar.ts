bb4c1cb08186db3742e85f6c567d9cc2a33326a4
Eddy Nguyen <github@eddeee888.me>
2024-08-07 21:46:41 +1000

    Update e2e tests

--- packages/typescript-resolver-files-e2e/src/test-resolvers-auto-wireup/schema/base/resolvers/CustomLogicScalar.ts
+++ packages/typescript-resolver-files-e2e/src/test-resolvers-auto-wireup/schema/base/resolvers/CustomLogicScalar.ts
@@ -1,3 +1,5 @@
+/* This file has been created on filesystem by src/test-resolvers/auto-wireup/test-setup.js */
+
 import { DateResolver} from 'graphql-scalars'
       DateResolver.description = undefined;
       export const CustomLogicScalar = DateResolver;
--- packages/typescript-resolver-files-e2e/src/test-resolvers-auto-wireup/schema/base/resolvers/JSON.ts
+++ packages/typescript-resolver-files-e2e/src/test-resolvers-auto-wireup/schema/base/resolvers/JSON.ts
@@ -1,3 +1,5 @@
+/* This file has been created on filesystem by src/test-resolvers/auto-wireup/test-setup.js */
+
 import { JSONResolver} from 'graphql-scalars'
       export const JSON = JSONResolver;
       
