59c3cee9611b0a189096f4f8b3f5db13a59a2cd4
Eddy Nguyen <github@eddeee888.me>
2024-09-03 21:39:12 +1000

    Set up e2e federation tests

---
+++ packages/typescript-resolver-files-e2e/src/test-federation/schema-base/subgraph-products/part-2/resolvers/Product.ts
@@ -0,0 +1,7 @@
+import type { ProductResolvers } from './../../types.generated';
+export const Product: Pick<
+  ProductResolvers,
+  'maker' | 'upc' | '__isTypeOf' | '__resolveReference'
+> = {
+  /* Implement Product resolver logic here */
+};
