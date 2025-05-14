68a4a2ebff3150def27bd4f0796a997df1a72cb5
Eddy Nguyen <github@eddeee888.me>
2025-05-14 22:08:37 +1000

    Update e2e tests

---
+++ packages/typescript-resolver-files-e2e/src/test-complex-synth-generic-wrapper/schema/resolvers.generated.ts
@@ -0,0 +1,9 @@
+/* This file was automatically generated. DO NOT UPDATE MANUALLY. */
+import type { Resolvers } from './types.generated';
+import { book as Query_book } from './base/resolvers/Query/book';
+import { Book } from './base/resolvers/Book';
+export const resolvers: Resolvers = {
+  Query: { book: Query_book },
+
+  Book: Book,
+};
