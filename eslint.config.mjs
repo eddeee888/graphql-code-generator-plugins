fc1e88698b71c04432c776ead683d4198d43e69e
Eddy Nguyen <ch@eddeee888.me>
2025-10-15 21:49:28 +1100

    Update ESLint config to use flat config (#399)
    * Fully convert to flat config
    
    * Bump eslint plugin
    
    * Fix eslint config
    
    * Fix lint issues, remove lint target from e2e projects

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,27 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import js from '@eslint/js';
-import nxEslintPlugin from '@nx/eslint-plugin';
+import { defineConfig } from 'eslint/config';
+import nx from '@nx/eslint-plugin';
+import my from '@eddeee888/eslint-plugin';
 
-const compat = new FlatCompat({
-  baseDirectory: dirname(fileURLToPath(import.meta.url)),
-  recommendedConfig: js.configs.recommended,
-});
-
-export default [
+export default defineConfig(
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+  ...my.configs.typescript,
   {
-    ignores: ['**/dist'],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
   },
-  { plugins: { '@nx': nxEslintPlugin } },
+  {
+    ignores: [
+      '**/dist',
+      'eslint.config.mjs',
+      'packages/typescript-resolver-files-e2e/src/**/*.generated.*',
+      'packages/typescript-resolver-files-e2e/src/**/*.gen.*',
+    ],
+  },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     rules: {
@@ -32,45 +40,6 @@ export default [
       ],
     },
   },
-  ...compat
-    .config({
-      extends: [
-        'plugin:@nx/typescript',
-        // FIXME: change to flat config
-        //'plugin:@eddeee888/typescript'
-      ],
-    })
-    .map((config) => ({
-      ...config,
-      files: ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts'],
-      rules: {
-        ...config.rules,
-      },
-    })),
-  ...compat
-    .config({
-      extends: ['plugin:@nx/javascript'],
-    })
-    .map((config) => ({
-      ...config,
-      files: ['**/*.js', '**/*.jsx', '**/*.cjs', '**/*.mjs'],
-      rules: {
-        ...config.rules,
-      },
-    })),
-  ...compat
-    .config({
-      env: {
-        jest: true,
-      },
-    })
-    .map((config) => ({
-      ...config,
-      files: ['**/*.spec.ts', '**/*.spec.tsx', '**/*.spec.js', '**/*.spec.jsx'],
-      rules: {
-        ...config.rules,
-      },
-    })),
   {
     files: ['**/*.json'],
     // Override or add rules here
@@ -78,11 +47,5 @@ export default [
     languageOptions: {
       parser: await import('jsonc-eslint-parser'),
     },
-  },
-  {
-    ignores: [
-      'packages/typescript-resolver-files-e2e/src/**/*.generated.*',
-      'packages/typescript-resolver-files-e2e/src/**/*.gen.*',
-    ],
-  },
-];
+  }
+);
--- packages/server-config/eslint.config.mjs
+++ packages/server-config/eslint.config.mjs
@@ -1,46 +1,22 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import js from '@eslint/js';
 import baseConfig from '../../eslint.config.mjs';
 
-const compat = new FlatCompat({
-  baseDirectory: dirname(fileURLToPath(import.meta.url)),
-  recommendedConfig: js.configs.recommended,
-});
-
 export default [
-  {
-    ignores: ['**/dist'],
-  },
   ...baseConfig,
-  {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    // Override or add rules here
-    rules: {},
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    // Override or add rules here
-    rules: {},
-    languageOptions: {
-      parserOptions: {
-        project: ['packages/server-config/tsconfig(.*)?.json'],
-      },
-    },
-  },
-  {
-    files: ['**/*.js', '**/*.jsx'],
-    // Override or add rules here
-    rules: {},
-  },
   {
     files: ['**/*.json'],
     rules: {
-      '@nx/dependency-checks': 'error',
+      '@nx/dependency-checks': [
+        'error',
+        {
+          ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs,ts,cts,mts}'],
+        },
+      ],
     },
     languageOptions: {
       parser: await import('jsonc-eslint-parser'),
     },
+  },
+  {
+    ignores: ['**/out-tsc'],
   },
 ];
